68760dcd93ea5437d55b5ce4d2ea8e8666216473
Jose Varela <joselcvarela@gmail.com>
2024-07-11 15:28:55 +0100

    Fix base url on request app

--- request/vite.config.ts
+++ request/vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/apps/home/dist" : "/",
+  base: process.env.NODE_ENV === "production" ? "/apps/request/dist" : "/",
   root: "src",
   build: {
     outDir: "../dist",
